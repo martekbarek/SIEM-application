50b3bd719df541c8b8626d3c5bcee519a8839098
bmarek <bartek.marek40@gmail.com>
2022-10-31 15:44:02 +0100

    Connect with database

---
+++ App/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python speedy7/manage.py flush --no-input
+python speedy7/manage.py migrate
+
+exec "$@"
