ae70564b65803b3d5c52b2df67a57d0df22b750f
bmarek <bartek.marek40@gmail.com>
2022-11-14 16:40:09 +0100

    Create simple dashboard

--- App/speedy7/core/models.py
+++ App/speedy7/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Log(models.Model):
+    datetime = models.DateTimeField()
+    facility = models.CharField(max_length=20)
+    level = models.CharField(max_length=20)
+    host = models.CharField(max_length=32)
+    program = models.CharField(max_length=20)
+    pid = models.CharField(max_length=8)
+    message = models.CharField(max_length=255)
+    
--- App/speedy7/core/views.py
+++ App/speedy7/core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Log
 
 # Create your views here.
-def index(request):
-    return HttpResponse("HELLOOOOO")
+def dashboard(response):
+    
+    all_logs = Log.objects.all
+    
+    return render(response, 'speedy/dashboard.html', {"all_logs":all_logs})
