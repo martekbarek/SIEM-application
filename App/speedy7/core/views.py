f43f2e860606dc9be0bb091bb2f966b81b687409
Marek <bartosz.marek@assaabloy.com>
2022-11-19 16:12:50 +0100

    Add last activity table

--- App/speedy7/core/views.py
+++ App/speedy7/core/views.py
@@ -6,24 +6,28 @@ from .filters import LogFilter
 # Create your views here.
 def dashboard(response):
     
-    all_logs = Log.objects.all
+    all_logs = Log.objects.order_by('-datetime').all()
     
     myFilter = LogFilter()
     
+    facilities = set(item.facility for item in all_logs)
+   
+    fac_counter_dict = {}
+    for facility in facilities:
+        counter = 0
+        for log in all_logs:
+            if facility in log.facility:
+                counter+=1
+        fac_counter_dict[facility]=counter        
+       
+    hosts = set(item.host for item in all_logs)
+    last_activity_dict = {}
+    for name in hosts:
+        last_activity_dict[name]=Log.objects.order_by('-datetime').filter(host=name).first()
     
     
-    context = {"all_logs":all_logs, 'myFilter':myFilter}
+      
+    
+    context = {"all_logs":all_logs, "fac_counter_dict":fac_counter_dict ,"last_activity_dict":last_activity_dict, 'myFilter':myFilter}
     
     return render(response, 'speedy/dashboard.html', context)
-
-
-def dashboardFilter(request):
-    
-    all_logs = Log.objects.all
-    
-    myFilter = LogFilter(request.GET, queryset=all_logs)
-    all_logs = myFilter.qs
-    
-    context = {"all_logs":all_logs, 'myFilter':myFilter}
-    
-    return render(request, 'speedy/dashboard.html', context)
