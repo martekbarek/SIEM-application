2f441b07cbc73f71c5200a467cb244df8bb248f3
Marek <bartosz.marek@assaabloy.com>
2022-11-19 14:22:44 +0100

    Add simple chart

--- App/speedy7/speedy7/settings.py
+++ App/speedy7/speedy7/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'chartjs',
     
     "core.apps.CoreConfig",
     'django_filters',
