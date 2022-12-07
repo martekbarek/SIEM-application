a9f5aed7123e78fc12bd2a3a065ede61970a7ca8
Marek <bartosz.marek@assaabloy.com>
2022-12-07 13:06:42 +0100

    Collect static files

--- App/entrypoint.sh
+++ App/entrypoint.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
 python speedy7/manage.py makemigrations
-# fake migrations for already created tables
 python speedy7/manage.py migrate --fake-initial
 python speedy7/manage.py createsuperuser --no-input
+python speedy7/manage.py collectstatic --no-input --clear
 
 exec "$@"
